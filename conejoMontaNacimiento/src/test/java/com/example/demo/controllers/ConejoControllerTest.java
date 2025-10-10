7fe353756215ee1f252f1f917a8a9c7f50314d9b
Juan Galindo <xjuangalindox@gmail.com>
2025-10-09 23:41:46 -0600

    GLFS-41 obtenerConejos(int, String, String, Model)

--- conejoMontaNacimiento/src/test/java/com/example/demo/controllers/ConejoControllerTest.java
+++ conejoMontaNacimiento/src/test/java/com/example/demo/controllers/ConejoControllerTest.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*; // Agregar manualmente (get)
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -24,10 +26,13 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import com.example.demo.clients.RazaClient;
 import com.example.demo.controllers.dto.ConejoDTO;
 import com.example.demo.controllers.dto.RazaDTO;
 import com.example.demo.models.ConejoModel;
 import com.example.demo.services.ConejoServiceImpl;
+import com.example.demo.services.IConejoService;
+import com.example.demo.util.ArchivoUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(ConejoController.class)
@@ -39,37 +44,87 @@ public class ConejoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    // @Autowired
-    // private ConejoController conejoController;
+    // @MockitoBean
+    // private ConejoServiceImpl conejoService;
 
     @MockitoBean
-    private ConejoServiceImpl conejoService;
+    private ArchivoUtil archivoUtil;
 
-    // Instancia beforeEach
-    private ConejoDTO semental, panda, mexicana, peluchin, pelusa, peluchina;
-    private RazaDTO minilop, leon;
+    @MockitoBean
+    private RazaClient razaClient;
+
+    @MockitoBean
+    private IConejoService conejoService;
+
+    // 
+    RazaDTO minilop;
+    ConejoDTO semental, panda, rocko, enojona;
 
     @BeforeEach
     void setup(){
-        // Razas
         minilop = new RazaDTO(1L, "Minilop");
-        leon = new RazaDTO(2L, "Cabeza de León");
-
-        // Conejos minilop
+        
         semental = new ConejoDTO(1L, null, null, null, "Semental", "Macho", null, false, 
-        "Primer semental de la granja", "123abc", "https://cloudinary.com/semental.png", null, null, null, minilop);
+            "Primer semental de la granja", "123abc", "https://cloudinary.com/semental.png", null, null, null, minilop);
         panda = new ConejoDTO(2L, null, null, null, "Panda", "Hembra", null, false, 
-        "Abuelita, jubilada", "123abc", "https://cloudinary.com/panda.png", null, null, null, minilop);
-        mexicana = new ConejoDTO(3L, null, null, null, "Mexicana", "Hembra", null, true, 
-        "Primera satinada", "123abc", "https://cloudinary.com/mexicana.png", null, null, null, minilop);
+            "Reproductora mas grande en la granja", "123abc", "https://cloudinary.com/panda.png", null, null, null, minilop);
+        rocko = new ConejoDTO(3L, null, null, null, "Rocko", "Macho", null, true, 
+            "Malito del ojo", "123abc", "https://cloudinary.com/rocko.png", null, null, null, minilop);
+        enojona = new ConejoDTO(4L, null, null, null, "Enojona", "Hembra", null, true, 
+        "Mordelona de la granja", "123abc", "https://cloudinary.com/enojona.png", null, null, null, minilop);
+    }
 
-        // Conejos leones
-        peluchin = new ConejoDTO(4L, null, null, null, "Peluchin", "Macho", null, true, 
-        "Semental de la granja estado", "123abc", "https://cloudinary.com/peluchin.png", null, null, null, leon);
-        pelusa = new ConejoDTO(5L, null, null, null, "Pelusa", "Hembra", null, true, 
-        "Hermana del semental de la granja", "123abc", "https://cloudinary.com/pelusa.png", null, null, null, leon);
-        peluchina = new ConejoDTO(6L, null, null, null, "Peluchina", "Hembra", null, true, 
-        "Hoja del semental de la granja", "123abc", "https://cloudinary.com/peluchina.png", null, null, null, leon);
+    @Test
+    void testObtenerConejos_filterBySexo() throws Exception{
+        // given
+        List<ConejoDTO> lista = List.of(semental, rocko);
+        Page<ConejoDTO> pageConejos = new PageImpl<>(lista);
+
+        // when
+        when(conejoService.findBySexo(anyInt(), anyInt(), anyString(), anyString())).thenReturn(pageConejos);
+
+        ResultActions response = mockMvc.perform(get("/conejos")
+            .param("pagina", "0")
+            .param("sexo", "Macho")
+            .param("ordenarPor", "nombre"));
+
+        response.andExpect(status().isOk())
+            .andDo(print())
+            .andExpect(view().name("conejos/lista"))
+            // .andExpect(model().attribute("pagina", 0))
+            .andExpect(model().attribute("sexo", "Macho"))
+            .andExpect(model().attribute("ordenarPor", "nombre"))
+            .andExpect(model().attribute("listaConejos", pageConejos.getContent()))
+            .andExpect(model().attribute("paginaActual", 0))
+            .andExpect(model().attribute("totalPaginas", pageConejos.getTotalPages()))
+            .andExpect(model().attribute("totalElementos", pageConejos.getTotalElements()));
+
+        verify(conejoService).findBySexo(anyInt(), anyInt(), anyString(), anyString());
+    }
+
+    @Test
+    void testobtenerConejos_filterByDefault() throws Exception{
+        List<ConejoDTO> lista = Arrays.asList(enojona, panda, rocko, semental);
+        Page<ConejoDTO> pageConejos = new PageImpl<>(lista);
+
+        when(conejoService.findAll(anyInt(), anyInt(), anyString())).thenReturn(pageConejos);
+
+        ResultActions response = mockMvc.perform(get("/conejos")
+            .param("pagina", "0")
+            .param("sexo", "")
+            .param("ordenarPor", ""));
+
+        response.andExpect(status().isOk())
+            .andDo(print())
+            .andExpect(view().name("conejos/lista"))
+            .andExpect(model().attribute("sexo", ""))
+            .andExpect(model().attribute("ordenarPor", "nombre"))
+            .andExpect(model().attribute("listaConejos", pageConejos.getContent()))
+            .andExpect(model().attribute("paginaActual", 0))
+            .andExpect(model().attribute("totalPaginas", pageConejos.getTotalPages()))
+            .andExpect(model().attribute("totalElementos", pageConejos.getTotalElements()));
+
+        verify(conejoService, times(1)).findAll(anyInt(), anyInt(), anyString());
     }
 
     // @Test
