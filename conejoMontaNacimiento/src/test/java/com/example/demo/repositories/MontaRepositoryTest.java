9c2896f5229ed8886cb1e85730c1ef17fb7c09bc
xjuangalindox <112209600+xjuangalindox@users.noreply.github.com>
2025-10-14 20:37:04 -0600

    Update MontaRepositoryTest.java
    Se elimino un import duplicado.
    import org.springframework.data.domain.Pageable.

--- conejoMontaNacimiento/src/test/java/com/example/demo/repositories/MontaRepositoryTest.java
+++ conejoMontaNacimiento/src/test/java/com/example/demo/repositories/MontaRepositoryTest.java
@@ -19,7 +19,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Pageable;
 
 import com.example.demo.models.ConejoModel;
 import com.example.demo.models.MontaModel;
